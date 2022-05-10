a9d9187a4bd455acd4edffdc4fee7bad0dca43c7
himanshu <himanshurai07417@gmail.com>
2022-05-10 13:46:35 +0530

    creating navbar component

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './navbar.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+
+
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav>
+      <a href=""><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 
